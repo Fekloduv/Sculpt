90529ac36d7546a1b55c5f0461c3d7fb1242104d
Anatoly <romafeklo@gmail.com>
2024-05-01 16:05:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sculpte/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sculpte;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button prof_button = findViewById(R.id.prof_button);
+        prof_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Запуск новой активити с макетом profile_window.xml
+                Intent intent = new Intent(MainActivity.this, profile.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
